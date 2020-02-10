a34171f5aced14ddec44656aa30a69a54482eb59
nothile <nothile.ngcobo@umuzi.org>
2020-02-10 11:59:57 +0200

    refactored soec and code

--- spec/test.js
+++ spec/test.js
@@ -1,10 +1,11 @@
  const visitor = require('../src/app')
 
-//  let addNewVisitor = visitor.addNewVisitor;
 
-//  let obj = {
-//      visitor_name: "name"
-//  };
+
+  let obj = {
+    visitor_name: "name",
+    visitor_age: "age"
+  };
 
  describe("add function to add visitor data", ()=>{
     beforeEach(function () {
@@ -13,12 +14,20 @@
 
     it("save data to str_col Column", async (done)=>{
        visitor.addNewVisitor("name");
-        // visitor = await addNewVisitor("name", null);
-        // done();
-        // expect(visitor[0].visitor_name).toEqual(obj.visitor_name)
+        
         expect(visitor.addNewVisitor).toHaveBeenCalled();
         expect(visitor.addNewVisitor).toHaveBeenCalledWith("name");
         done();
+    });
+
+
+    it('add visitor information', async (done) => {
+        visitor = await addNewVisitor("age");
+
+         expect(visitor.addNewVisitor).toHaveBeenCalled();
+         expect(visitor[0].visitor_age).toEqual(obj.visitor_age);
+         done();
+
     });
 
     
--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const pool = new Pool({
        port: process.env.DB_PORT,
        user:process.env.DB_USER,
        password: process.env.DB_PASSWORD,
-       database: process.env.DB_umuzi_visitors
+       database: process.env.DB_DB
 });
 
 const createTable = async () => {
